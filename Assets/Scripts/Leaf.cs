1f989b19b3049ef8962e527464065a189340c454
Marko Jovanovic <artist.olives@gmail.com>
2023-07-02 16:53:07 -0700

    Change cursor with FontAwesome icons

--- Assets/Scripts/Leaf.cs
+++ Assets/Scripts/Leaf.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Leaf : MonoBehaviour
 {
+    [SerializeField] Texture2D CursorPointer;
     [SerializeField] Material highlightMaterial;
     Material originalMaterial;
     MeshRenderer meshRenderer;
-    // Start is called before the first frame update
     void Start()
     {
         meshRenderer = gameObject.GetComponent<MeshRenderer>();
@@ -17,10 +17,12 @@ public class Leaf : MonoBehaviour
     private void OnMouseEnter()
     {
         meshRenderer.material = highlightMaterial;
+        Cursor.SetCursor(CursorPointer, new Vector2(175,0), CursorMode.Auto);
     }
 
     private void OnMouseExit()
     {
         meshRenderer.material = originalMaterial;
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
 }
